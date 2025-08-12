a9c74b024f02abb255196fde15fad6a3fefbc2d4
dyno-nexsoft <dyno-nexsoft@users.noreply.github.com>
2025-08-12 02:10:40 +0000

    deploy: eb0e85037c6c6443dfa7fe784275645c5ac8ecbf

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "85159f512cef530c6572762d51d50b47",
+const RESOURCES = {"flutter_bootstrap.js": "df8074c86332d84f5c27cbf1cca01e9a",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
