c86606a516e9aec5c107a80e1a2bb2e4d354d46b
dyno-nexsoft <dyno-nexsoft@users.noreply.github.com>
2025-08-12 08:54:02 +0000

    deploy: 70ad38e846b12911e58ddc8793f093c61c973f41

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "91dd9451bc2878c068799b9d03dae28a",
+const RESOURCES = {"flutter_bootstrap.js": "5fdc658f97950883bee674739395ab29",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
@@ -21,7 +21,7 @@ const RESOURCES = {"flutter_bootstrap.js": "91dd9451bc2878c068799b9d03dae28a",
 "canvaskit/skwasm.js.symbols": "e72c79950c8a8483d826a7f0560573a1",
 "canvaskit/canvaskit.wasm": "7a3f4ae7d65fc1de6a6e7ddd3224bc93",
 "canvaskit/canvaskit.js.symbols": "bdcd3835edf8586b6d6edfce8749fb77",
-"main.dart.js": "10e6f2f1b5e12dedcda7716670558d62",
+"main.dart.js": "fd21cc6ae9b6ff459e992cb5a1cd77ac",
 "index.html": "81cc4f3d079d0050ed93dea6d380aba0",
 "/": "81cc4f3d079d0050ed93dea6d380aba0",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
